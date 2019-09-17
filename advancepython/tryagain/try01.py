763db0b518e431dcf2d1b1ccf7781b02523be841
Steven Keller <dr.keller83@gmail.com>
2019-09-17 13:34:10 -0400

    completed try except lab

---
+++ advancepython/tryagain/try01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Start with an infinite loop
+while True:
+    try:
+        print('Enter a file name: ')
+        name = input()
+        myfile = open(name, 'w')
+        myfile.close()
+        break
+    except:
+        print('Error with that file name! Try again...')
+        
