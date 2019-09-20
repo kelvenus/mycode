b83bbb65c32af4075dcca854b2a9c50670fff0ac
Steven Keller <dr.keller83@gmail.com>
2019-09-20 09:16:52 -0400

    Created flasks forms, used to import planet data from swapi.io and pass it to a form

---
+++ advancepython/flaskapi/flask_form.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, url_for, render_template
+import requests
+app = Flask(__name__)  
+
+@app.route('/planets')
+def planets():
+    data = []
+    for i in range(1, 11):
+        planetapi = requests.get(f'https://swapi.co/api/planets/{i}/?format=json')
+        getplanet = planetapi.json()
+        data.append({'name': getplanet['name'], 'climate': getplanet['climate'], 'gravity': getplanet['gravity']})
+
+    #print(data[f'name': 'climate'])
+    print(data) 
+    return render_template('planets.html', planetname = data)
+	
+if __name__ == '__main__':
+   app.run(port=5006, host='0.0.0.0')
